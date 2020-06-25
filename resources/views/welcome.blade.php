6f063377ae41ed537b6634a6eb2fe0800a7e3f23
pedro lucca <pedrolucca257@gmail.com>
2020-06-24 22:55:07 -0300

    Alterando nome home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Alterando nome home page
                 </div>
 
                 <div class="links">
